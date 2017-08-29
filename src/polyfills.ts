465e25c6e63c5d82c01476b785f7323b9f8f7bab
inpercima <inpercima@gmail.com>
2017-08-29 22:31:46 +0200

    Include intl to run app on mobile devices

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,6 +2,11 @@ import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 
+//https://github.com/angular/angular-cli/issues/1675#issuecomment-254027916
+// works in production mode only
+import 'intl';
+import 'intl/locale-data/jsonp/en';
+
 if (process.env.ENV === 'development') {
   Error['stackTraceLimit'] = Infinity;
   require('zone.js/dist/long-stack-trace-zone');
