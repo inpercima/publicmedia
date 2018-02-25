c578f4c762beb61b42a4b15a73651bb81c92e696
inpercima <inpercima@gmail.com>
2018-02-25 17:37:49 +0100

    Remove webpack-de-server

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,10 +7,6 @@ let webpackMerge = require('webpack-merge');
 const ENV = process.env.NODE_ENV = process.env.ENV = 'development';
 
 module.exports = webpackMerge(commonConfig, {
-  devServer: {
-    // to open the local server in browser
-    contentBase: __dirname + '/build',
-  },
   plugins: [
     // tell angular to be in development mode
     new webpack.DefinePlugin({
