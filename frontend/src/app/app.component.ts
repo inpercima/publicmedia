d5952d13a0d8791dfd529e56c45f6f117d3b031d
inpercima <inpercima@gmail.com>
2024-02-24 07:22:44 +0100

    fix(): Fix handling a theme
    Before background and other styles were not correctly set.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { OverlayContainer } from '@angular/cdk/overlay';
-import { Component, HostBinding, TemplateRef } from '@angular/core';
+import { Component, Inject, TemplateRef } from '@angular/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { Title } from '@angular/platform-browser';
 
+import { DOCUMENT } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { environment } from '../environments/environment';
@@ -18,14 +18,10 @@ import { DashboardComponent } from './features/dashboard/dashboard.component';
 export class AppComponent {
   public appname: string;
 
-  // Adds the custom theme to the app root.
-  @HostBinding('class') class = `${environment.theme}-theme`;
-
-  public constructor(private dialog: MatDialog, private titleService: Title, public overlayContainer: OverlayContainer) {
+  public constructor(private dialog: MatDialog, private titleService: Title, @Inject(DOCUMENT) private document: Document) {
     this.appname = environment.appname;
     this.titleService.setTitle(this.appname);
-    // Adds the custom theme to dialogs.
-    this.overlayContainer.getContainerElement().classList.add(`${environment.theme}-theme`);
+    this.document.body.classList.add(`${environment.theme}-theme`);
   }
 
   openDialog(ref: TemplateRef<any>): void {
