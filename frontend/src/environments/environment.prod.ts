b0ca1da30df11624dd43f383666d5abfdf590d2b
inpercima <inpercima@gmail.com>
2024-03-10 14:18:50 +0100

    chore(): enable prod config files

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  api: './api/rest/',
+  appname: 'publicmedia',
+  defaultRoute: 'dashboard',
+  production: false,
+  theme: 'indigo-pink',
+  userId: '25025320',
+  username: 'instagram',
+};
