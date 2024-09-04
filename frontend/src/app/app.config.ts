48398ff3c8dd642cb7ada67ad2ea671c9a2698e6
Marcel Hendrich <inpercima@gmail.com>
2024-09-04 14:59:44 +0200

    chore(): add provideZoneChangeDetection

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideAnimations()],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(), provideAnimations()],
 };
