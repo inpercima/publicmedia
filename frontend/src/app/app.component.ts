df2930733410ed5eda54efd1b8f1d19dbe040821
Marcel Hendrich <inpercima@gmail.com>
2025-12-28 14:36:14 +0100

    chore(deps): update @angular/cli to 20.3.13 and its dependencies

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component, Inject, TemplateRef } from '@angular/core';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { Title } from '@angular/platform-browser';
-
-import { DOCUMENT } from '@angular/common';
+import { Component, DOCUMENT, Inject, TemplateRef } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Title } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 
@@ -17,11 +15,7 @@ import { DashboardComponent } from './features/dashboard/dashboard.component';
 export class AppComponent {
   public appname: string;
 
-  public constructor(
-    private dialog: MatDialog,
-    private titleService: Title,
-    @Inject(DOCUMENT) private document: Document,
-  ) {
+  public constructor(private dialog: MatDialog, private titleService: Title, @Inject(DOCUMENT) private document: Document) {
     this.appname = environment.appname;
     this.titleService.setTitle(this.appname);
     this.document.body.classList.add(`${environment.theme}-theme`);
